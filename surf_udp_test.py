a79d025068e49bae244d1b3ac188a1c53cd40572
Pierre Tinard <pierre@tinard.fr>
2020-04-02 16:12:49 +0200

    uplink

--- surf_udp_test.py
+++ surf_udp_test.py
@@ -2,10 +2,11 @@ import os
 import subprocess
 import time
 import threading
+import random
 from selenium import webdriver
 
-# Protocole à utiliser pour saturer l'uplink (TCP, UDP ou None pour désactiver l'uplink)
-UPLINK_PROTOCOL = "UDP"
+# Protocole à utiliser pour saturer l'uplink (TCP, UDP ou None pour désactiver l'uplink. ALL pour tester les 3)
+UPLINK_PROTOCOL = "ALL"
 # Nombre de tests à effectuer par site web
 NB_OF_TESTS = 1
 
@@ -23,7 +24,7 @@ def create_tmp_file():
 
 def launch_curl_uplink(probe=False):
     if probe:
-        args = "--max-time 15"
+        args = "--max-time 10"
         output = subprocess.PIPE
     else: 
         args = ""
@@ -34,12 +35,8 @@ def launch_curl_uplink(probe=False):
         while True:
             time.sleep(2)
             if cmd.poll() != None:
-                if cmd.poll() == 28:
-                    break
-                elif cmd.poll() == 7:
-                    raise Exception("curl_closed")
-                elif cmd.poll() != 0:
-                    raise Exception("curl_error")
+                if cmd.returncode == -15:
+                    return
                 break
         if probe:
             break
@@ -51,13 +48,30 @@ def launch_curl_uplink(probe=False):
     return rate
 
 def launch_iperf_uplink(maxrate_kbps):
-# TODO
-    cmd = os.popen(
-        "curl -4 -o /dev/null -F 'filecontent=@/tmp/3000M_tmp.iso' http://bouygues.testdebit.info")
+    i = 0
+    while i < 30:
+        port = random.randrange(9200, 9223)
+        cmd = subprocess.Popen("iperf3 -4 -u -c paris.testdebit.info -p "+str(port)+" -b "+str(maxrate_kbps)+"k -t 5000 -i 5",
+                               shell=True)
+
+        time.sleep(2)
+        if cmd.poll() == None:
+            success = True
+            print("iPerf launched")
+            break
+        else:
+            print("Retrying other port...")
+
+        i = i+1
 
 def stop_curl_uplink():
     cmd = os.popen(
         "killall curl")
+
+
+def stop_iperf_uplink():
+    cmd = os.popen(
+        "killall iperf3")
 
 def run_browsing_tests():
     urls = [
@@ -90,31 +104,56 @@ def run_browsing_tests():
         
 
 
-def main():
-    if UPLINK_PROTOCOL == "TCP":
+def launch_tests(ulproto):
+    if ulproto == "TCP":
+        print("Testing surf performance with TCP upload")
         create_tmp_file()
         print("Launching continuous TCP upload...")
         curlUplinkThread = threading.Thread(target=launch_curl_uplink)
         curlUplinkThread.setDaemon(True)
         curlUplinkThread.start()
-        time.sleep(2.5)
+        time.sleep(4)
         print("")
 
-    elif UPLINK_PROTOCOL == "UDP":
-        print("Polling max uplink rate for 15 seconds...")
-        maxrate_kbps = launch_curl_uplink(True)
+    elif ulproto == "UDP":
+        print("Testing surf performance with UDP upload")
+        print("Polling max uplink rate for 10 seconds...")
+        maxrate_kbps = round(launch_curl_uplink(True)*1.25)
+        print(maxrate_kbps)
         print("Launching continuous UDP upload at "+str(maxrate_kbps)+" kbps...")
-        curlUplinkThread = threading.Thread(target=launch_iperf_uplink, args=(maxrate_kbps,))
-        curlUplinkThread.setDaemon(True)
-        curlUplinkThread.start()
+        launch_iperf_uplink(maxrate_kbps)
         time.sleep(2.5)
         print("")
+    else:
+        print("Testing surf performance without uplink stress")
 
-    # results = run_browsing_tests()
-    # print("")
-    # for url, result in results.items():
-    #     print("Avg complete loading time for "+url+" : "+str(result)+" seconds")
+    results = run_browsing_tests()
+    if ulproto == "TCP":
+        stop_curl_uplink()
+    elif ulproto == "UDP":
+        stop_iperf_uplink()
 
-    stop_curl_uplink()
+    time.sleep(2)
 
-main()
+    print("")
+    print("")
+    for url, result in results.items():
+        displayresult.append("== Avg "+url+" ("+ulproto+" UL stress) : "+str(result)+" seconds")
+    displayresult.append("")
+    return displayresult
+
+if __name__ == "__main__":
+    displayresult = []
+    if UPLINK_PROTOCOL == "ALL":
+        launch_tests("None")
+        print("")
+        time.sleep(6)
+        launch_tests("TCP")
+        print("")
+        time.sleep(6)
+        launch_tests("UDP")
+    else:
+        launch_tests(UPLINK_PROTOCOL)
+
+    for result in displayresult:
+        print(result)
